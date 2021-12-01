8677f1bcb7d62620a6871769a93e466d01afab12
kinder85 <kinder1985@hotmail.fr>
2021-12-01 07:52:16 +0100

    Add files via upload

--- testpad.py
+++ testpad.py
@@ -3,15 +3,9 @@ import sys
 import time
 import os
 import socket
-
-HOST = '192.168.8.223'
-PORT = 80
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-print ('Connexion vers ' + HOST + ':' + str(PORT) + ' reussie.')
-
-
+import paho.mqtt.client as mqtt
+clientm = mqtt.Client()
+clientm.connect("192.168.8.111",1883,60)
            # Joystick button number to perform an EPO reset (Start)
 buttonSlow = 4                         # Joystick button number for driving slowly whilst held (L2)
 buttonBoost = 5                        # Joystick button number for speed boost whilst held (L1)
@@ -22,7 +16,9 @@ if __name__ == '__main__':
     
     
     
-    
+    clientm.loop_start()
+    mde = "joy"
+    clientm.publish("mode", mde)
     pygame.init()
     pygame.joystick.init()
     joystick = pygame.joystick.Joystick(0)
@@ -31,6 +27,7 @@ if __name__ == '__main__':
     hadEvent = False
     
     while True:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 # User exit
@@ -45,26 +42,33 @@ if __name__ == '__main__':
                 
                 if (joystick.get_axis(2) > 0.5):
                             """"If the hat returns tuple of 1,0 hat is pressed to the right"""
-                            client.send(b"3,%d,%d." % (vmax,delay))
+                            move = (b"3,%d,%d." % (vmax,delay))
                             print("droite")
+                            clientm.publish("pre", move)
                 
                 
                 elif (joystick.get_axis(2) < -0.5):
                             """"If the hat gives returns tuple of -1,0 hat is pressed left"""
-                            client.send(b"4,%d,%d." % (vmax,delay))
+                            move = (b"4,%d,%d." % (vmax,delay))
                             print("gauche")
+                            clientm.publish("pre", move)
                 
                 elif (joystick.get_axis(1) < -0.5):
                             """"If the hat returns tuple of 0,1 hat is pressed up"""
-                            client.send(b"1,%d,%d." % (vmax,delay))
+                            move = (b"1,%d,%d." % (vmax,delay))
                             print("up")
+                            clientm.publish("pre", move)
                 
                 elif (joystick.get_axis(1) > 0.5):
                             """"If the hat gives returns tuple of 0,-1 hat is pressed down"""
-                            client.send(b"2,%d,%d." % (vmax,delay))
+                            move = (b"2,%d,%d." % (vmax,delay))
                             print("down")
+                            clientm.publish("pre", move)
+                
                 else:
-                   client.send(b"5,%d,%d." % (vmax,delay)) 
+                   move = (b"5,%d,%d." % (vmax,delay))
+                   clientm.publish("pre", move)
+              
                 if joystick.get_button(4):
                     vmax = 100
                     
@@ -78,11 +82,8 @@ if __name__ == '__main__':
             
                 #print (di,vmax,delay)
                 
-        # Change the LED to reflect the status of the EPO latch
-        
-        # Wait for the interval period
-        #time.sleep(0.1)
-    # Disable all drives
-    
+       
+        time.sleep(0.1)
+   
   
     
