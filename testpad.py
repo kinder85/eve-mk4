941a10bf8a9592bb311e737a6d8318f2271613b0
kinder85 <kinder1985@hotmail.fr>
2021-12-01 09:06:45 +0100

    Add files via upload

--- testpad.py
+++ testpad.py
@@ -9,16 +9,31 @@ clientm.connect("192.168.8.111",1883,60)
            # Joystick button number to perform an EPO reset (Start)
 buttonSlow = 4                         # Joystick button number for driving slowly whilst held (L2)
 buttonBoost = 5                        # Joystick button number for speed boost whilst held (L1)
-
+pad = 0
 vmax = 150
 delay = 0
+mod = "manuel"
+
+
+def on_message_mode(mosq, obj, msg):
+        global mod
+        global pad
+        mod = msg.payload.decode("utf-8").strip()
+        if mod != "joy":
+            pad = 0
+        
+#         print(mod)
+        
+            
+
+
+
 if __name__ == '__main__':
     
     
     
-    clientm.loop_start()
-    mde = "joy"
-    clientm.publish("mode", mde)
+    
+    
     pygame.init()
     pygame.joystick.init()
     joystick = pygame.joystick.Joystick(0)
@@ -27,42 +42,59 @@ if __name__ == '__main__':
     hadEvent = False
     
     while True:
+        clientm.loop_start()
+        clientm.message_callback_add('mode', on_message_mode)
+        clientm.subscribe("mode", 0)
+        if mod != "joy" and pad == 1:
+            
+            mde = "joy"
+            clientm.publish("mode", mde)
+        
         
         for event in pygame.event.get():
+            
             if event.type == pygame.QUIT:
                 # User exit
                 running = False
             elif event.type == pygame.JOYBUTTONDOWN:
                 # A button on the joystick just got pushed down
                 hadEvent = True
+                
             elif event.type == pygame.JOYAXISMOTION:
                 # A joyst*ick has been moved
                 hadEvent = True
-            if hadEvent:
                 
+            else:
+                hadEvent = False
+                
+            
+            print(pad)
+            if hadEvent:
+                pad = 1
+         
                 if (joystick.get_axis(2) > 0.5):
                             """"If the hat returns tuple of 1,0 hat is pressed to the right"""
                             move = (b"3,%d,%d." % (vmax,delay))
-                            print("droite")
+#                             print("droite")
                             clientm.publish("pre", move)
                 
                 
                 elif (joystick.get_axis(2) < -0.5):
                             """"If the hat gives returns tuple of -1,0 hat is pressed left"""
                             move = (b"4,%d,%d." % (vmax,delay))
-                            print("gauche")
+#                             print("gauche")
                             clientm.publish("pre", move)
                 
                 elif (joystick.get_axis(1) < -0.5):
                             """"If the hat returns tuple of 0,1 hat is pressed up"""
                             move = (b"1,%d,%d." % (vmax,delay))
-                            print("up")
+#                             print("up")
                             clientm.publish("pre", move)
                 
                 elif (joystick.get_axis(1) > 0.5):
                             """"If the hat gives returns tuple of 0,-1 hat is pressed down"""
                             move = (b"2,%d,%d." % (vmax,delay))
-                            print("down")
+#                             print("down")
                             clientm.publish("pre", move)
                 
                 else:
@@ -81,9 +113,9 @@ if __name__ == '__main__':
                 #client.send(b"%d;%d;%d" % (di,vmax,delay))                
             
                 #print (di,vmax,delay)
-                
+            
        
-        time.sleep(0.1)
+#         time.sleep(0.1)
    
   
     
